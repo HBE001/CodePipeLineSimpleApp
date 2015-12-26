4d0c665535564e122950a36ba619cf5e9b020e64
Yahya from Local <yahya@funwavegames.com>
2015-12-26 20:27:54 +0200

    Running hook scripts with access to pm2

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 
 #!/bin/sh
 cd ~
-sudo pm2 start /home/lotusinterworks.internal/yahyas/simpleApp/app.js
+pm2 start /home/lotusinterworks.internal/yahyas/simpleApp/app.js
--- stop.sh
+++ stop.sh
@@ -2,4 +2,4 @@
 touch log_file
 echo whoami >> log_file
 cd /home/lotusinterworks.internal/yahyas/codedeploy
-sudo pm2 stop all
+pm2 stop all
