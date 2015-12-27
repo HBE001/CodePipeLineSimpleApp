52e8e75dbe111944c9f74a3ac9943ea6505dc9bd
Yahya from Local <yahya@funwavegames.com>
2015-12-27 22:36:47 +0200

    Testing CodePipeline and CodeDeploy on Platforms500

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 cd /home/lotusinterworks.internal/yahyas/simpliadeployment
 sudo chmod +x setup_nginx_debug_repo.sh
-sudo ./setup_nginx_debug_repo.sh yahya 3001 3002
+sudo ./setup_nginx_debug_repo.sh HelloWorldApp 3001 3002
 sudo pm2 start app.js --node-args="--debug=3002"
+
+cd simplia
+export NODE_PATH=../node_modules
+sudo ./setup_nginx_debug_repo.sh SimpliaApp 3006 3007
+sudo pm2 start index.js --node-args="--debug=3007"
+
