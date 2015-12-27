425ee101180b6c6a65c07ad6ed9074e05f1b6dbe
Yahya from Local <yahya@funwavegames.com>
2015-12-27 22:27:56 +0200

    Testing CodePipeline and CodeDeploy on Platforms500

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd /home/lotusinterworks.internal/yahyas/simpliadeployment
-sudo pm2 start app.js
+sudo chmod +x setup_nginx_debug_repo.sh
+sudo ./setup_nginx_debug_repo.sh yahya 3001 3002
+sudo pm2 start app.js --node-args="--debug=3002"
