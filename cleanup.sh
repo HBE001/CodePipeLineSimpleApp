a56318f6bda3b117a71518cc321ec823a8179b85
Yahya from Local <yahya@funwavegames.com>
2015-12-28 00:50:09 +0200

    Testing CodePipeline and CodeDeploy on Platforms500

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /home/lotusinterworks.internal/yahyas
-sudo mv --backup=existing simpliadeployment simpliadeployment_old
+#sudo mv --backup=existing simpliadeployment simpliadeployment_old
+sudo rm -R simpliadeployment
