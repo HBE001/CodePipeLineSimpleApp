dbb32a079e70c1394f6eef0cc3ac784dbcc7e90d
Yahya from Local <yahya@funwavegames.com>
2015-12-28 03:01:58 +0200

    Testing CodePipeline and CodeDeploy on Platforms500

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!, Second Version, THis is test');
+  res.send('Hello World!, Second Version, THis is test 2!');
 });
 
 var server = app.listen(3000, function () {
